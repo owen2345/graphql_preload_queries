39d0a035414dde8e863e99d3865988a7e92490f6
owen2345 <owenperedo@gmail.com>
2021-01-21 12:41:52 -0400

    fix: fix deep stack recursive error

--- config/initializers/patch_continue_value.rb
+++ config/initializers/patch_continue_value.rb
@@ -1,20 +1,18 @@
 # frozen_string_literal: true
 
-Rails.application.config.to_prepare do
-  GraphQL::Execution::Interpreter::Runtime.class_eval do
-    alias_method :continue_value_old, :continue_value
-    # gql args: path, value, parent_type, field, is_non_null, ast_node
-    def continue_value(*args)
-      value = args[1]
-      ast_node = args[5]
-      field = args[3]
-      type_klass = field.owner
-      if !value.is_a?(ActiveRecord::Relation) || value.loaded? || !type_klass.respond_to?(:preloads)
-        return continue_value_old(*args)
-      end
+require 'graphql/execution/interpreter/runtime'
+module GraphqlPreloadQueries::PatchContinueValue # rubocop:disable Style/ClassAndModuleChildren:
+  # gql args: path, value, parent_type, field, is_non_null, ast_node
+  def continue_value(*args)
+    value = args[1]
+    ast_node = args[5]
+    field = args[3]
+    type_klass = field.owner
+    is_active_record = value.is_a?(ActiveRecord::Relation)
+    return super if !is_active_record || value.loaded? || !type_klass.respond_to?(:preloads)
 
-      klass = GraphqlPreloadQueries::Extensions::Preload
-      klass.preload_associations(value, ast_node, type_klass)
-    end
+    klass = GraphqlPreloadQueries::Extensions::Preload
+    klass.preload_associations(value, ast_node, type_klass)
   end
 end
+GraphQL::Execution::Interpreter::Runtime.prepend GraphqlPreloadQueries::PatchContinueValue
