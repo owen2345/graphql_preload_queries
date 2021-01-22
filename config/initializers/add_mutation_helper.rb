9a2e816922d1fd43e8c4cab03091b755f82012d6
owen2345 <owenperedo@gmail.com>
2021-01-22 09:56:30 -0400

    feat: auto camelize keys for queries and mutations

--- config/initializers/add_mutation_helper.rb
+++ config/initializers/add_mutation_helper.rb
@@ -9,6 +9,7 @@ Rails.application.config.to_prepare do
     # @param collection (ActiveCollection)
     # @param type_klass (GQL TypeClass)
     def include_gql_preloads(gql_result_key, collection, type_klass = nil)
+      gql_result_key = GraphQL::Schema::Member::BuildType.camelize(gql_result_key.to_s)
       type_klass ||= preload_type_klass(gql_result_key.to_s)
       klass = GraphqlPreloadQueries::Extensions::Preload
       ast_node = preload_find_node(gql_result_key)
